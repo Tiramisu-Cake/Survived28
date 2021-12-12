8dbb8fe5327b6ff4daef6180405db5757a7b6275
Tiramisu-Cake <95933273+Tiramisu-Cake@users.noreply.github.com>
2021-12-13 02:04:11 +0300

    modified 2nd task

--- Level1.java
+++ Level1.java
@@ -23,7 +23,7 @@ public class Level1 {
 
     public static int odometer(int [] oksana) {
         int S = oksana[0]*oksana[1];
-        for(int i = 2; i<oksana.length; i = i + 2) {
+        for(int i = 2; i<oksana.length; i += 2) {
             S += oksana[i] * (oksana[i+1] - oksana[i-1]);
         }
         return S;
