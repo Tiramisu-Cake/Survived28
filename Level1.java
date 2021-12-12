f26e85ff126d7dcc0c449dacd722f7f173f2c54b
Igor Kharin <igkharin@gmail.com>
2021-12-12 14:52:43 +0300

    add 1st prelim task

---
+++ Level1.java
@@ -0,0 +1,23 @@
+package com.company;
+public class Level1 {
+    public static int squirrel(int N) {
+        if(N == 0) {
+            return 1;
+        }
+        double log_Frac = 0.0;
+        for(int i = 2; i <= N; i++) {
+            log_Frac += Math.log10(i); // sum of logarithms of numbers 2..N
+        }
+
+        // Taking the fraction part of sum
+        double int_part = (int) log_Frac;
+        log_Frac = log_Frac - int_part;
+
+        // fimding the first digit of N!
+        double z = Math.pow(10,log_Frac);
+        int x = (int) z;
+        while(x >= 10)
+            x /= 10;
+        return x;
+    }
+}
