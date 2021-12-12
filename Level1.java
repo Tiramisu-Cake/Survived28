f0381226203cbfa4dcab5ee5247b10104787c96f
Tiramisu-Cake <95933273+Tiramisu-Cake@users.noreply.github.com>
2021-12-12 16:42:19 +0300

    added 2nd prelim task

--- Level1.java
+++ Level1.java
@@ -20,4 +20,11 @@ public class Level1 {
             x /= 10;
         return x;
     }
+   public static int odometer(int [] oksana) {
+        int S = oksana[0]*oksana[1];
+        for(int i = 2; i<oksana.length; i = i + 2) {
+            S += oksana[i] * (oksana[i+1] - oksana[i-1]);
+        }
+        return S;
+    }  
 }
