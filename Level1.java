ab133424dfd5f7ae78f40a7f3e70dbc1eb341f8b
Igor Kharin <igkharin@gmail.com>
2021-12-28 21:53:08 +0300

    Finished (with corrections) 27th task

--- Level1.java
+++ Level1.java
@@ -1424,6 +1424,10 @@ public class Level1 {
             }
         }
 
+        if (Arrays.equals(Ideal, F)) {
+            return false;
+        }
+
         int p = 0;
         for (int i = 0; i < N -1; i++) {
             for (int j = i+1; j < N; j++) {
